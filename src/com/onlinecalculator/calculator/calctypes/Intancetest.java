bbae6a42893cfafa809adb603ae346586c771e43
istar <istar@192.168.100.20>
2017-05-25 17:54:01 +0530

    Implementing a local variable without using object..

---
+++ src/com/onlinecalculator/calculator/calctypes/Intancetest.java
@@ -0,0 +1,16 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.Scanner;
+public class Intancetest {
+ static void display()
+ {
+  int x=10;
+  Scanner s=new Scanner (System.in);
+  System.out.println("local variable x="+x);
+}
+public static void main (String args [])
+{
+	display();
+}
+  
+  
+}
