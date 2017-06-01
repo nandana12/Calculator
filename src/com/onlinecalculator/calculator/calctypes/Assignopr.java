8c1a7b0e587e49f59f1e0bad5627c751028240e9
ADMIN <ADMIN@192.168.0.7>
2017-06-01 11:08:52 +0530

    Assignment operator program by NANDINI..

---
+++ src/com/onlinecalculator/calculator/calctypes/Assignopr.java
@@ -0,0 +1,29 @@
+package com.onlinecalculator.calculator.calctypes;
+
+import java.util.Scanner;
+
+public class Assignopr {
+	public static void main (String args [])
+	{
+		int a,b;
+		 Scanner s=new Scanner(System.in);
+		 System.out.println("enter first no");
+	    	a=s.nextInt();	
+	    	System.out.println ("enter second  no");
+	    	b=s.nextInt();
+
+	    	System.out.println(a+=b);
+	    	System.out.println(a-=b);
+	    	System.out.println(a*=b);
+	    	System.out.println(a/=b);
+	    	System.out.println(a%=b);
+	    	System.out.println(a<<=b);
+	        System.out.println(a>>=b);
+	    	System.out.println(a&=b);
+	    	System.out.println(a^=b);
+	    	System.out.println(a|=b);
+	   }
+	    
+}
+
+
